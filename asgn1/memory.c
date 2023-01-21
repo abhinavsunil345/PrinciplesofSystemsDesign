ac37a86558e6c3a6532e8443b4b1693eef1a91db
Abhinav Sunil <aksunil@ucsc.edu>
2023-01-20 20:48:25 -0800

    23/25

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -4,46 +4,65 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#define _POSIX_C_SOURCE 200809L
 
 int main (){
 	int fd;
 	char str [800];
-	char str2 [800];
+	char str2 [8000];
 	char str3 [8000];
-	char buf[4096];
+	char buf[10000];
 	char buf1;
 	int count = 0;
+	int sp = 0;
 	int bytes_read = 0;
 	// char* token;
 	while (read(0, &buf1, 1) == 1) {
 		if (buf1 == '\n') {
+		sp++;
 		break;
 		}
 		str3[count] = buf1;
 		count++;
 	}
+	if (sp != 1) {
+		fprintf( stderr, "Invalid Command\n");
+		return 1;
+	}
 	char *token = strtok(str3, " \t");
 	
         strcpy(str, token);
         token = strtok(NULL, " \t");
+        if (token == NULL) {
+        fprintf( stderr, "Invalid Command\n");
+          return 1;
+		}
         strcpy(str2, token);
-	 printf("%s\n", str);
-	// printf("%s\n", str3);s
-	printf("%s\n", str2);
+	// printf("%s\n", str);
+	// printf("%s\n", str3);
+	// printf("%s\n", str2);
 	
 	if (strcmp(str, "get") != 0 && strcmp(str, "set") != 0) {
 		fprintf( stderr, "Invalid Command\n");
 		exit(1);
 	}
-	if (strcmp(str, "get") == 0) { 
-		fd = open (str2, O_RDWR);
-	if (fd == -1) {
+	
+	
+	if (strcmp(str, "get") == 0) {
+		int getcheck = read(0, &buf1, 1);
+		if (getcheck != 0) {
 		fprintf( stderr, "Invalid Command\n");
 		return 1;
 	}
+		fd = open (str2, O_RDWR);
+	if (fd == -1) {
+		fprintf( stderr, "Invalid Command\n");
+		close(fd);
+		return 1;
+	}
 	do {
-	   bytes_read = read(fd, buf, 4096);
-	   printf("%d", bytes_read);
+	   bytes_read = read(fd, buf, 10000);
+	  // printf("%d", bytes_read);
 	   if (bytes_read < 0) {
 	   fprintf( stderr, "Invalid Command\n");
 		return 1;
@@ -62,17 +81,22 @@ int main (){
 	close(fd);
 }
 if (strcmp(str, "set") == 0) { 
+		 
+	
 		fd = open (str2, O_RDWR|O_TRUNC);
 	//	printf("%d", fd);
 	if (fd == -1) {
 		fd = open(str2, O_RDWR|O_CREAT|O_TRUNC, 0777);
+		if( read(fd, &buf1, 1) == -1)
+  	{
+    		fprintf( stderr, "Invalid Command\n");
+            return 1;
+  	}
 	}
-	else {
-		fprintf( stdout, "OK");
-	}
+		
 	do {
-	   bytes_read = read(0, buf, 4096);
-	   printf("%d", bytes_read);
+	   bytes_read = read(0, buf, 10000);
+	  // printf("%d", bytes_read);
 	   if (bytes_read < 0) {
 	   fprintf( stderr, "Invalid Command\n");
 		return 1;
@@ -92,6 +116,7 @@ if (strcmp(str, "set") == 0) {
 	//while (read(0, &buf, 4096) == 4096) { 
 	//	write(fd, &buf, 4096);
 	//}
+	fprintf( stdout, "OK\n");
 	close(fd);
 }
 
