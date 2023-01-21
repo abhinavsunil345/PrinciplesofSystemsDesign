606d21f841ca38842d001814939dbe50a9dad297
Abhinav Sunil <aksunil@ucsc.edu>
2023-01-20 16:08:27 -0800

    changed everything

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -9,10 +9,25 @@ int main (){
 	int fd;
 	char str [800];
 	char str2 [800];
-	char buf;
+	char str3 [8000];
+	char buf[4096];
+	char buf1;
+	int count = 0;
+	int bytes_read = 0;
 	// char* token;
+	while (read(0, &buf1, 1) == 1) {
+		if (buf1 == '\n') {
+		break;
+		}
+		str3[count] = buf1;
+		count++;
+	}
 	scanf("%s", str);
+	// printf("%s\n", str);
+	printf("%s\n", str3);
 	scanf("%s", str2);
+	//printf("%s\n", str2);
+	
 	if (strcmp(str, "get") != 0 && strcmp(str, "set") != 0) {
 		fprintf( stderr, "Invalid Command\n");
 		exit(1);
@@ -21,22 +36,59 @@ int main (){
 		fd = open (str2, O_RDWR);
 	if (fd == -1) {
 		fprintf( stderr, "Invalid Command\n");
-		exit(1);
+		return 1;
 	}
-	while (read(fd, &buf, 1) == 1) { 
-		printf("%c", buf);
-	}
+	do {
+	   bytes_read = read(fd, buf, 4096);
+	   printf("%d", bytes_read);
+	   if (bytes_read < 0) {
+	   fprintf( stderr, "Invalid Command\n");
+		return 1;
+		}
+		else if (bytes_read > 0) { 
+		int bytes_written = 0;
+		do {
+			int bytes = write(1, buf + bytes_written, bytes_read - bytes_written);
+			if (bytes <= 0) {
+			fprintf( stderr, "Invalid Command\n");
+			}
+			bytes_written += bytes;
+			} while (bytes_written < bytes_read);
+			
+	}} while (bytes_read > 0);
 	close(fd);
 }
 if (strcmp(str, "set") == 0) { 
-		fd = open (str2, O_RDWR);
+		fd = open (str2, O_RDWR|O_TRUNC);
 	//	printf("%d", fd);
 	if (fd == -1) {
 		fd = open(str2, O_RDWR|O_CREAT|O_TRUNC, 0777);
 	}
-	while (read(0, &buf, 1) == 1) { 
-		write(fd, &buf, 1);
+	else {
+		fprintf( stdout, "OK");
 	}
+	do {
+	   bytes_read = read(0, buf, 4096);
+	   printf("%d", bytes_read);
+	   if (bytes_read < 0) {
+	   fprintf( stderr, "Invalid Command\n");
+		return 1;
+		}
+		else if (bytes_read > 0) { 
+		int bytes_written = 0;
+		do {
+			int bytes = write(fd, buf + bytes_written, bytes_read - bytes_written);
+			if (bytes <= 0) {
+			fprintf( stderr, "Invalid Command\n");
+			}
+			bytes_written += bytes;
+			} while (bytes_written < bytes_read);
+			
+	}} while (bytes_read > 0);
+	
+	//while (read(0, &buf, 4096) == 4096) { 
+	//	write(fd, &buf, 4096);
+	//}
 	close(fd);
 }
 
